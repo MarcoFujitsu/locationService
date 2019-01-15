c0b7a374f5dd391de32bc24bd1719de6a38e152e
Littel <marco.littel@ts.fujitsu.com>
2019-01-15 08:56:00 +0100

    smal bugfix in result generation

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/address', (req, res) => {
         
         var result = '{ "lat" : "' + myItem.geometry.lat + '", "lng" : "' + myItem.geometry.lng + '"}';
         // result = '{  "replies": [ { "type": "text","content": "lat: ' + myItem.geometry.lat + ' lng: ' + myItem.geometry.lng + ' " } ] } ';
-        result = '{    "type": "card",    "content": {      "title": "Your location",      "subtitle": "LOCATION",      "imageUrl": "http://open.mapquestapi.com/staticmap/v4/getmap?key=ENYNqiKYXC843j4cnbcezq4awv1UI5tB&size=400,400&zoom=16&center=52.0532588,4.4985999&pois=red_1,52.0532588,4.4985999,0,0",      "buttons": [      ]    }  }'
+        result = '{    "type": "card",    "content": {      "title": "Your location",      "subtitle": "LOCATION",      "imageUrl": "http://open.mapquestapi.com/staticmap/v4/getmap?key=ENYNqiKYXC843j4cnbcezq4awv1UI5tB&size=400,400&zoom=16&center=52.0532588,4.4985999&pois=red_1,52.0532588,4.4985999,0,0",      "buttons": ["buttons": [        {          "title": "BUTTON_TITLE",          "type": "postback",          "value": "BUTTON_VALUE"        }      ]    }  }';
         res.send(result);
       }  
     });
