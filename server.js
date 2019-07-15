69dba4cfb3e6035c315a7c31766b1903c6f8e8ac
Littel <marco.littel@ts.fujitsu.com>
2019-07-15 13:10:37 +0200

    small fixes and tweaks

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+let convert = {};
 var express = require('express');
 var app = express();
 var request = require('request');
@@ -5,86 +6,91 @@ var unirest = require('unirest');
 var bodyParser = require("body-parser");
 var fs = require('fs');
 
-
-app.use(bodyParser.urlencoded({ extended: false }));
+-
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 app.post("/address", (req, res) => {
+  console.log('address request');
   const url = 'https://api.opencagedata.com';
-    
-    var key = process.env.geolocationkey;
-    const path = '/geocode/v1/json?key=' + key +  '&pretty=1&q=';
-   
-    var data = encodeURIComponent(req.query["address"]); 
-    var myBody =req.body.address;
-    var data = encodeURIComponent(myBody);
 
-    unirest.get(url+path+data)
-    .end(function(response) {
-      
-      var answer = response.body;   
-      
-      var myItem = getHighestConfidenceItem(answer.results);
-    
-      if(myItem!=null) {
-        
+  var key = process.env.geolocationkey;
+  const path = '/geocode/v1/json?key=' + key + '&pretty=1&q=';
+
+  var data = encodeURIComponent(req.query["address"]);
+  var myBody = req.body.address;
+  var data = encodeURIComponent(myBody);
+
+  unirest.get(url + path + data)
+    .end(function (response) {
+
+      var answer = response.body;
+
+      var myItem = convert.getHighestConfidenceItem(answer.results);
+
+      if (myItem != null) {
+
         var result = '{ "lat" : "' + myItem.geometry.lat + '", "lng" : "' + myItem.geometry.lng + '"}';
         result = '{  "replies": [ { "type": "text","content": "lat: ' + myItem.geometry.lat + ' lng: ' + myItem.geometry.lng + ' " } ] } ';
-                
-        a = { replies: [
-          {
-            type: 'quickReplies',
-            content: {
-              title : 'lat: ' + myItem.geometry.lat + ' lng: ' + myItem.geometry.lng 
+
+        a = {
+          replies: [
+            convert.GenerateQuickReply('lat: ' + myItem.geometry.lat + ' lng: ' + myItem.geometry.lng)
+
+          ],
+          conversation: {
+            language: "en",
+            memory: {
+              lat: myItem.geometry.lat,
+              lng: myItem.geometry.lng
             }
           }
-          
-        ],
-        conversation : {
-          language: "en",
-          memory : {
-            lat : myItem.geometry.lat,
-            lng : myItem.geometry.lng
-          }
+        }
+
+      } else {
+        a = {
+          replies: [{
+            "type": "text",
+            "content": "No location found"
+          }]
         }
       }
       res.send(a);
-        
-/*        
-        res.send(
-          {
-            type: 'quickReplies',
-            content: {
-              title: 'Sorry, but I could not find any results for your request',
-              buttons: [{ title: 'Start over', value: 'Start over' }]
-            }
-          }
-        );
-
-*/
-        
-        /*result = '{    "type": "card",    "content": {      "title": "Your location",      "subtitle": "LOCATION",      "imageUrl": "http://open.mapquestapi.com/staticmap/v4/getmap?key=ENYNqiKYXC843j4cnbcezq4awv1UI5tB&size=400,400&zoom=16&center=52.0532588,4.4985999&pois=red_1,52.0532588,4.4985999,0,0",	"buttons": [        {          "title": "BUTTON_TITLE",          "type": "postback",          "value": "BUTTON_VALUE"        }      ]    }  }';
-        console.log(result);
-        res.send(result); */
-      }  
     });
 });
 
-function getHighestConfidenceItem(results)
-{
-    var result;
-    var highestConfidence = 0;
-  
-    for(i=0;i<results.length;i++) {
-      if( results[i].confidence > highestConfidence)
-      {
-        result = results[i];
-        highestConfidence = results[i].confidence;
-
-      }
-      
-      return result;
+convert.GenerateQuickReply = function (content) {
+  var result = {
+    type: 'quickReplies',
+    content: {
+      title: content
     }
+  };
+  return result;
+}
+
+convert.GenerateMemory = function (content) {
+  var result = {};
+
+}
+
+convert.getHighestConfidenceItem = function (results)
+// function getHighestConfidenceItem(results)
+{
+  var result;
+  var highestConfidence = 0;
+
+  for (i = 0; i < results.length; i++) {
+    if (results[i].confidence > highestConfidence) {
+      result = results[i];
+      highestConfidence = results[i].confidence;
+
+    }
+
+    return result;
+  }
 
 }
 
@@ -93,3 +99,5 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
+
+module.exports = convert;
--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
+let convert =require("../server.js");
 var assert = require("assert");
+
 describe("Array", function() {
   describe("#indexOf()", function() {
     it("should return -1 when the value is not present", function() {
@@ -6,3 +8,42 @@ describe("Array", function() {
     });
   });
 });
+
+describe("Math", function () {
+  it("should return true if value is 9", function() {
+      assert.equal(9, 3*3);
+  });
+
+  it("shoudl return true if value is -8", function() {
+    assert.equal(-8, (3-4)*8);
+  })
+}
+);
+
+describe("Determineconfidence", function() {
+  it("should return the highest confidence item of 98", function() {
+      var results = [ 
+        { confidence: 98, name: "test98"} ,
+        { confidence: 97, name: "test97"} ,
+        { confidence: 94, name: "test94"} 
+      ]
+
+      assert.equal(convert.getHighestConfidenceItem(results).confidence,  98);
+
+  });
+
+});
+
+describe("QuickReply", function () {
+  it("should generate quickreply with hello world",  function() {
+      var a = convert.GenerateQuickReply("hello world");
+      var result = {
+        type: 'quickReplies',
+        content: {
+          title : "hello world"
+        }
+      };
+      assert.equal(a.content.title,result.content.title);
+  });
+});
+
